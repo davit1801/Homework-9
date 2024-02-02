f53c0dbb27e904620c5961b2ebb7162d3b05b874
davit1801 <155184127+davit1801@users.noreply.github.com>
2024-02-02 14:14:42 +0400

    update

---
+++ index.js
@@ -0,0 +1,59 @@
+// 1 
+
+// const arr = [1,2,3,4,5,];
+
+// for (let element of arr) {
+//     console.log(element);
+// }
+
+
+
+
+
+
+//-------------------------------
+//2 
+
+// function display(x,secondfunc){
+//     return secondfunc(x);
+// }
+
+// function shefasebisfunc(price){
+//     if(price > 50) {
+//         console.log("ნივთის ფასი ძვირია");
+//     } else if (price >= 20 && price <= 50){
+//         console.log("ნივთის ფასი საშუალოა");
+//     } else {
+//         console.log("ნივთის ფასი იაფია");
+//     }
+// }
+
+// display (20,shefasebisfunc)
+
+
+
+
+
+
+
+
+
+
+// --------------------------------------------
+
+//3 
+
+// const array = [15,53,22,198,10,28,16,70,33,951];
+
+// function maxNumber (arr) {
+//     let result = -Infinity;
+//     for(let element of array) {
+//         if(element > result){
+//             result = element;
+//         }
+//     }
+//     return result;
+// }
+
+// console.log(maxNumber(array));
+
